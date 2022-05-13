52f328d7234327f5f693a362b284770ff4713659
vaishnavi <105497778+vaishnavidevi25@users.noreply.github.com>
2022-05-13 18:30:12 +0530

    Update streamlit_app.py
    added extra items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit
 streamlit.title("My Parent's New Healthty Diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Omega 3 & Blueberry oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
