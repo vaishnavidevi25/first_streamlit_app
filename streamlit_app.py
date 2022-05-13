08452195cc4287670adaeb8fdc64b2b6e3d504a0
vaishnavi <105497778+vaishnavidevi25@users.noreply.github.com>
2022-05-13 18:26:59 +0530

    Update streamlit_app.py
    imported streamlit
    added title
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title("My Parent's New Healthty Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
